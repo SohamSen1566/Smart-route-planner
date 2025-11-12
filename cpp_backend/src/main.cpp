05aac7cac9b8f7a2c2ce8de3fcc46e86021fcc8d
SohamSen1566 <sohamsen1566@gmail.com>
2025-11-13 00:32:39 +0530

    test input

--- cpp_backend/src/main.cpp
+++ cpp_backend/src/main.cpp
@@ -1,1 +1,26 @@
-//soham
+#include "route_manager.hpp"
+#include "utils.hpp"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    RouteManager rm;
+
+    rm.addRoute("Pune", "Mumbai", 150);
+    rm.addRoute("Mumbai", "Nashik", 180);
+    rm.addRoute("Nashik", "Nagpur", 700);
+    rm.addRoute("Pune", "Bangalore", 850);
+    rm.addRoute("Bangalore", "Hyderabad", 570);
+
+    rm.displayCities();
+
+    string src, dest;
+    cout << "Enter source city: ";
+    cin >> src;
+    cout << "Enter destination city: ";
+    cin >> dest;
+
+    rm.findShortestRoute(src, dest);
+
+return 0;
+}
