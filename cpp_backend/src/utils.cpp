3896a67a1501fde33b3118313c57369890b61df4
Aditya Mahesh Chhajed <adityachhajed62@gmail.com>
2025-11-13 02:03:01 +0530

    updated the utils files

--- cpp_backend/src/utils.cpp
+++ cpp_backend/src/utils.cpp
@@ -1,7 +1,10 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 //aditya
 =======
 >>>>>>> 8f69d57 (added utils.cpp and utils.hpp files with utility functions)
+=======
+>>>>>>> 19ee08c (updated the utils files)
 #include "utils.hpp"
 
 string Utils::toLower(string s) {
