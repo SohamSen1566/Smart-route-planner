8f69d574989c9145fa4148e07e5c9db282076853
SohamSen1566 <sohamsen1566@gmail.com>
2025-11-13 01:31:14 +0530

    added utils.cpp and utils.hpp files with utility functions

---
+++ cpp_backend/include/utils.hpp
@@ -0,0 +1,13 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Utils {
+public:
+    static string toLower(string s);
+    static void printDivider();
+};
+
+#endif
--- cpp_backend/src/utils.cpp
+++ cpp_backend/src/utils.cpp
@@ -1,1 +1,10 @@
-//aditya
+#include "utils.hpp"
+
+string Utils::toLower(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    return s;
+}
+
+void Utils::printDivider() {
+    cout << "----------------------------------------\n";
+}
