e90cd6326752574e44b3957e11e83a65d43327b5
Arpit <b24cs1015@iitj.ac.in>
2025-11-13 00:05:08 +0530

    updated the graphs file

--- cpp_backend/src/graph.cpp
+++ cpp_backend/src/graph.cpp
@@ -1,1 +1,20 @@
 // arpit 
+#include "graph.hpp"
+
+Graph::Graph(int n) : numVertices(n) {
+    adjList.resize(n);
+}
+
+void Graph::addEdge(int u, int v, double weight) {
+    if (u >= numVertices || v >= numVertices) return;
+    adjList[u].push_back({v, weight});
+    adjList[v].push_back({u, weight}); // undirected
+}
+
+const vector<pair<int, double>>& Graph::getNeighbors(int u) const {
+    return adjList[u];
+}
+
+int Graph::size() const {
+    return numVertices;
+}
